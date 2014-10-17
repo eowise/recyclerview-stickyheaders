6b183bb7bf54f6cda37fd82902bd4ccfdc07bfff
aurel <aurelien.catinon@gmail.com>
2014-10-17 21:27:37 +0200

    Samples : Use support toolbar

--- samples/src/main/java/com/eowise/recyclerview/stickyheaders/samples/MainActivity.java
+++ samples/src/main/java/com/eowise/recyclerview/stickyheaders/samples/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
-import android.widget.Toolbar;
 
 import com.eowise.recyclerview.stickyheaders.HeaderPosition;
 import com.eowise.recyclerview.stickyheaders.InvalidateAnimationRunnable;
