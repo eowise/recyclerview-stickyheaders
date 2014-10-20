f01f18058c4251a088ae0cfc85c022b25e3d4231
aurel <aurelien.catinon@gmail.com>
2014-10-20 18:52:49 +0200

    Remove animations in builder

--- library/src/main/java/com/eowise/recyclerview/stickyheaders/StickyHeaderBuilder.java
+++ library/src/main/java/com/eowise/recyclerview/stickyheaders/StickyHeaderBuilder.java
@@ -8,14 +8,12 @@ import android.support.v7.widget.RecyclerView;
  */
 public class StickyHeaderBuilder {
 
-    private Boolean isAnimationEnabled;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private StickyHeadersAdapter headersAdapter;
     private HeaderPosition headerPosition;
 
     public StickyHeaderBuilder() {
-        this.isAnimationEnabled = false;
     }
 
     public StickyHeaderBuilder setRecyclerView(RecyclerView recyclerView) {
@@ -38,33 +36,11 @@ public class StickyHeaderBuilder {
         return this;
     }
 
-    public StickyHeaderBuilder enableAnimations() {
-        this.isAnimationEnabled = true;
-        return this;
-    }
-
     public StickyHeadersItemDecoration build() {
 
         // TODO: Throw if adapter doesn't have stables ids
 
         StickyHeadersItemDecoration decoration =  new StickyHeadersItemDecoration(headersAdapter, recyclerView, headerPosition);
-
-
-        if (isAnimationEnabled) {
-            adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-
-                @Override
-                public void onItemRangeInserted(int positionStart, int itemCount) {
-                    ViewCompat.postOnAnimationDelayed(recyclerView, new InvalidateAnimationRunnable(recyclerView), 50);
-                }
-
-                @Override
-                public void onItemRangeRemoved(int positionStart, int itemCount) {
-                    ViewCompat.postOnAnimationDelayed(recyclerView, new InvalidateAnimationRunnable(recyclerView), 50);
-                }
-            });
-        }
-
 
         decoration.registerAdapterDataObserver(adapter);
 
