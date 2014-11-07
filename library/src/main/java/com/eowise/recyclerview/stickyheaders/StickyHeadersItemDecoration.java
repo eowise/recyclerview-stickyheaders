89f446281b1140b36708a42df0cdd2b27e45d3df
aurel <aurelien.catinon@gmail.com>
2014-11-07 22:09:35 +0100

    Fix for multiple same headers when adding items (again)

--- library/src/main/java/com/eowise/recyclerview/stickyheaders/StickyHeadersItemDecoration.java
+++ library/src/main/java/com/eowise/recyclerview/stickyheaders/StickyHeadersItemDecoration.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerViewHelper;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -78,8 +79,10 @@ public class StickyHeadersItemDecoration extends RecyclerView.ItemDecoration {
 
         RecyclerView.LayoutParams lp = (RecyclerView.LayoutParams)view.getLayoutParams();
         RecyclerView.ViewHolder holder = parent.getChildViewHolder(view);
+        boolean isHeader = lp.isItemRemoved() ? headerStore.wasHeader(holder) : headerStore.isHeader(holder);
 
-        if (overlay || !headerStore.isHeader(holder)) {
+
+        if (overlay || !isHeader) {
             outRect.set(0, 0, 0, 0);
         }
         else {
@@ -129,12 +132,14 @@ public class StickyHeadersItemDecoration extends RecyclerView.ItemDecoration {
     private class HeaderStore {
 
         private final HashMap<Long, View> headersViewByHeadersIds;
-        private final HashMap<Long, Boolean> isHeadersByItemsIds;
+        private final HashMap<Long, Boolean> wasHeaderByItemId;
+        private final ArrayList<Boolean> isHeaderByItemPosition;
         private final HashMap<Long, Integer> headersHeightsByItemsIds;
 
         public HeaderStore() {
             this.headersViewByHeadersIds = new HashMap<Long, View>();
-            this.isHeadersByItemsIds = new HashMap<Long, Boolean>();
+            this.wasHeaderByItemId = new HashMap<Long, Boolean>();
+            this.isHeaderByItemPosition = new ArrayList<Boolean>();
             this.headersHeightsByItemsIds = new HashMap<Long, Integer>();
         }
 
@@ -170,57 +175,58 @@ public class StickyHeadersItemDecoration extends RecyclerView.ItemDecoration {
         }
 
         public boolean isHeader(RecyclerView.ViewHolder itemHolder) {
-
-            if (!isHeadersByItemsIds.containsKey(itemHolder.getItemId())) {
-                int itemPosition = RecyclerViewHelper.convertPreLayoutPositionToPostLayout(parent, itemHolder.getPosition());
-
-                isHeadersByItemsIds.put(itemHolder.getItemId(), itemPosition == 0 || adapter.getHeaderId(itemPosition) != adapter.getHeaderId(itemPosition - 1));
+            int itemPosition = RecyclerViewHelper.convertPreLayoutPositionToPostLayout(parent, itemHolder.getPosition());
+            if (isHeaderByItemPosition.size() <= itemPosition) {
+                isHeaderByItemPosition.add(itemPosition, itemPosition == 0 || adapter.getHeaderId(itemPosition) != adapter.getHeaderId(itemPosition - 1));
+            }
+            else if (isHeaderByItemPosition.get(itemPosition) == null) {
+                isHeaderByItemPosition.set(itemPosition, itemPosition == 0 || adapter.getHeaderId(itemPosition) != adapter.getHeaderId(itemPosition - 1));
             }
 
-            return isHeadersByItemsIds.get(itemHolder.getItemId());
+            return isHeaderByItemPosition.get(itemPosition);
+        }
+
+        public boolean wasHeader(RecyclerView.ViewHolder itemHolder) {
+            if (!wasHeaderByItemId.containsKey(itemHolder.getItemId())) {
+                int itemPosition = RecyclerViewHelper.convertPreLayoutPositionToPostLayout(parent, itemHolder.getPosition());
+                wasHeaderByItemId.put(itemHolder.getItemId(), itemPosition == 0 || adapter.getHeaderId(itemPosition) != adapter.getHeaderId(itemPosition - 1));
+            }
+            return wasHeaderByItemId.get(itemHolder.getItemId());
         }
 
         public void onItemRangeRemoved(int positionStart, int itemCount) {
-            RecyclerView.ViewHolder holder = parent.findViewHolderForPosition(positionStart + itemCount);
-            if (holder != null) {
-                isHeadersByItemsIds.remove(holder.getItemId());
-                headersHeightsByItemsIds.remove(holder.getItemId());
+
+            for (int i = 0; i < itemCount; i++) {
+                RecyclerView.ViewHolder holder = parent.findViewHolderForPosition(positionStart + i);
+                if (holder != null) {
+                    wasHeaderByItemId.put(holder.getItemId(),isHeaderByItemPosition.get(positionStart + i ));
+                }
             }
 
-            cleanOffScreenItemsIds();
+            if (isHeaderByItemPosition.size() > positionStart + itemCount) {
+                isHeaderByItemPosition.set(positionStart + itemCount, null);
+            }
+
+            for (int i = 0; i < itemCount; i++) {
+                isHeaderByItemPosition.remove(positionStart);
+            }
         }
 
         public void onItemRangeInserted(int positionStart, int itemCount) {
-            boolean isCleanOffScreenItemsIdsNeeded = false;
-            for (int i = 0; i <= itemCount; i++) {
-                RecyclerView.ViewHolder holder = parent.findViewHolderForPosition(positionStart + i);
-                if (holder != null) {
-                    isHeadersByItemsIds.remove(holder.getItemId());
-                    headersHeightsByItemsIds.remove(holder.getItemId());
-                }
-                else {
-                    isCleanOffScreenItemsIdsNeeded = true;
-                }
+            for (int i = 0; i < itemCount; i++) {
+                isHeaderByItemPosition.add(positionStart, null);
             }
 
-            if (isCleanOffScreenItemsIdsNeeded) {
-                cleanOffScreenItemsIds();
+            if (isHeaderByItemPosition.size() > positionStart + itemCount) {
+                isHeaderByItemPosition.set(positionStart + itemCount, null);
             }
         }
 
         public void clear() {
             headersViewByHeadersIds.clear();
-            isHeadersByItemsIds.clear();
+            isHeaderByItemPosition.clear();
+            wasHeaderByItemId.clear();
         }
 
-        private void cleanOffScreenItemsIds() {
-            Iterator<Long> iterator = isHeadersByItemsIds.keySet().iterator();
-            while (iterator.hasNext()) {
-                long itemId = iterator.next();
-                if (parent.findViewHolderForItemId(itemId) == null) {
-                    iterator.remove();
-                }
-            }
-        }
     }
 }
